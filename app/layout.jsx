bf6d4801754ef8b165b0623e9e3f488aadc46fe8
unknown <vlad.albu@oxidev.ro>
2025-12-17 23:23:32 +0200

    First commit

---
+++ app/layout.jsx
@@ -0,0 +1,16 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "DMS - CallCenter",
+  description: "In-house app",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className='antialiased'>
+        {children}
+      </body>
+    </html>
+  );
+}
